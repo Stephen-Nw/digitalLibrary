7a33526b3ffde92483abeef14203fc2889ad8a22
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-02-08 22:20:44 -0600

    add secret key

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from forms import LoginForm, RegisterForm
 import requests
+import os
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books-tracker.db"
+app.config['SECRET_KEY'] = os.environ['SecretKey']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # Bootstrap(app)
