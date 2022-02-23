8659df4d54c85051aa3c2285e162d7f7d698d37c
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-02-22 21:43:08 -0600

    update completed reads route

--- main.py
+++ main.py
@@ -235,18 +235,33 @@ def add_completed_book(book_id):
         new_book.book_title = book_data['volumeInfo']['title']
         new_book.book_author = ', '.join([str(item) for item in book_author])  # Convert author list to string
         new_book.image_url = book_data['volumeInfo']['imageLinks']['thumbnail']
-        try:
-            new_book.publish_date = book_data['volumeInfo']['publishedDate']
-        except KeyError:
-            new_book.publish_date = " "
-        else:
-            new_book.publish_date = book_data['volumeInfo']['publishedDate']
+        new_book.publish_date = book_data['volumeInfo']['publishedDate']
         new_book.category = "Completed"
+        new_book.user_id = current_user.id
         db.session.add(new_book)
         db.session.commit()
         return redirect(url_for('completed_reading'))
     elif book_in_db.category != "Completed":
         book_in_db.category = "Completed"
+        db.session.commit()
+        return redirect(url_for('completed_reading'))
+    elif book_in_db.user_id != current_user.id:  # add book to db if already in db under another user
+        response = requests.get(f"https://www.googleapis.com/books/v1/volumes/{book_id}")
+        response.raise_for_status()
+        book_data = response.json()
+
+        book_author = book_data['volumeInfo']['authors']  # This is a list that has to be converted into a string before
+        # adding to db
+
+        new_book = Book()
+        new_book.book_id = book_id
+        new_book.book_title = book_data['volumeInfo']['title']
+        new_book.book_author = ', '.join([str(item) for item in book_author])  # Convert author list to string
+        new_book.image_url = book_data['volumeInfo']['imageLinks']['thumbnail']
+        new_book.publish_date = book_data['volumeInfo']['publishedDate']
+        new_book.category = "Completed"
+        new_book.user_id = current_user.id
+        db.session.add(new_book)
         db.session.commit()
         return redirect(url_for('completed_reading'))
     else:
